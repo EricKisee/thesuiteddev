5185536444cb1019a2e3dc58bfd7e26db7f153a6
Eric Kisee <erickiseemulwa@gmail.com>
2021-01-07 13:47:51 +0300

    initial commit 3

--- frontend/static/js/views/PostView.js
+++ frontend/static/js/views/PostView.js
@@ -3,14 +3,17 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView{
     constructor(params){
         super(params);
-        this.setTitle("Dashboard");
+        this.setTitle("Viewing Posts");
     }
 
     async getHtml(){
+        const id = this.params.id;
+        console.log(id);
+
         return `
             <h1> Welcome Back </h1>
-            <p> This is the Suited dev blog</p>
-            
+            <p> Viewing a post from some unique day</p>
+            <a><a>
         `;
     }
 }
